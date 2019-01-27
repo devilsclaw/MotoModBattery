1e78dd5909307cf2ce3d4ca625e02fb5387563f8
dc <neilhellfeldt@gmail.com>
2019-01-27 12:12:45 -0800

    install.sh: make it install if it does not exist and add documentation

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#used for adb over the network
+#if it fails, it will attemp usb adb, I believe
 adb connect "${1}:5555"
+#This will fail after first successful install
+adb install app/build/outputs/apk/debug/app-debug.apk
+#after a successful install then do a replace install
 adb install -r app/build/outputs/apk/debug/app-debug.apk
+#launch the app
 adb shell monkey -p "com.devilsclaw.motomodbattery" -c android.intent.category.LAUNCHER 1
