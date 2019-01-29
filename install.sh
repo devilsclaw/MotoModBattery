7d52c761d5dbbd24f89cab4b166427b22e432b0c
dc <neilhellfeldt@gmail.com>
2019-01-28 19:36:34 -0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+ip="192.168.1.253"
+if [ "${1}" != "" ] ; then
+  ip="${1}"
+fi
+./gradlew :app:assembleDebug
 #used for adb over the network
 #if it fails, it will attemp usb adb, I believe
-adb connect "${1}:5555"
+adb connect "${ip}:5555"
 #This will fail after first successful install
-adb install app/build/outputs/apk/debug/app-debug.apk
+#adb install app/build/outputs/apk/debug/app-debug.apk
 #after a successful install then do a replace install
 adb install -r app/build/outputs/apk/debug/app-debug.apk
 #launch the app
